09239a3774473427f7223ce24abae7a6ca4a1cf7
Wellington Reis <wellington.costa.97@gmail.com>
2020-03-29 00:16:35 -0300

    Added unitary and integration tests using Jest

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,27 +1,27 @@
-const connection = require ('../database/connection');
-const crypto = require ('crypto');
+const connection = require("../database/connection");
+const uniqueID = require("../utils/generateUniqueID");
 
 module.exports = {
-    async index (request, response) {
-        const ongs = await connection('ongs').select('*');
-    
-        return response.json(ongs);
-    },
+	async index(request, response) {
+		const ongs = await connection("ongs").select("*");
 
-    async create(request, response) {
-        const { name, email, whatsapp, city, uf } = request.body;
+		return response.json(ongs);
+	},
 
-        const id = crypto.randomBytes(4).toString('HEX');
-    
-        await connection('ongs').insert({
-            id,
-            name,
-            email,
-            whatsapp,
-            city,
-            uf
-        })
-    
-        return response.json({ id })
-    }
-}
+	async create(request, response) {
+		const { name, email, whatsapp, city, uf } = request.body;
+
+		const id = uniqueID();
+
+		await connection("ongs").insert({
+			id,
+			name,
+			email,
+			whatsapp,
+			city,
+			uf
+		});
+
+		return response.json({ id });
+	}
+};
---
+++ backend/tests/unit/generateUniqueID.spec.js
@@ -0,0 +1,9 @@
+const generateUniqueID = require("../../src/utils/generateUniqueID");
+
+describe("Generate Unique ID", () => {
+	it("should generate an Unique ID", () => {
+		const id = generateUniqueID();
+
+		expect(id).toHaveLength(8);
+	});
+});
