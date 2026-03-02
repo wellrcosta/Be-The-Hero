a609630700a04e76c4994d89d67098ff8f26e9c4
Wellington Reis <wellington.costa.97@gmail.com>
2026-03-02 06:05:50 +0000

    chore(api): disconnect Prisma on module destroy

--- apps/api/src/prisma/prisma.service.ts
+++ apps/api/src/prisma/prisma.service.ts
@@ -1,14 +1,18 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/prisma/client";
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../generated/prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService extends PrismaClient implements OnModuleDestroy {
   constructor() {
     super({
       adapter: new PrismaPg({
         connectionString: process.env.DATABASE_URL,
       }),
     });
+  }
+
+  async onModuleDestroy() {
+    await this['$disconnect']();
   }
 }
