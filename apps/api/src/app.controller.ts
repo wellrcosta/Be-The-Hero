156ffd8cf4084cb060e50c19f90a95f22deb9f02
Wellington Reis <wellington.costa.97@gmail.com>
2026-03-02 05:34:33 +0000

    feat(api): redirect root to swagger docs

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Redirect } from '@nestjs/common';
+import { Public } from './auth/public.decorator';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
+  @Public()
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Redirect('/docs', 302)
+  redirectToDocs() {
+    return;
   }
 }
